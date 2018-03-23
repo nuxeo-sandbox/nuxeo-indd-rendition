7b6cd84c6d844a5accaac2796bce7ad56c0e6168
Michaël Vachette <mvachette@nuxeo.com>
2018-03-22 21:33:33 -0700

    fix listener

--- nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/listener/InddPreviewGeneration.java
+++ nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/listener/InddPreviewGeneration.java
@@ -3,51 +3,35 @@ package org.nuxeo.labs.indd.rendition.listener;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.event.Event;
-import org.nuxeo.ecm.core.event.EventBundle;
 import org.nuxeo.ecm.core.event.EventContext;
-import org.nuxeo.ecm.core.event.PostCommitFilteringEventListener;
+import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.labs.indd.rendition.worker.InddPreviewWorker;
 import org.nuxeo.runtime.api.Framework;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class InddPreviewGeneration implements PostCommitFilteringEventListener {
-  
-  protected final List<String> handled = Arrays.asList("aboutToCreate", "beforeDocumentModification");
-
-    @Override
-    public void handleEvent(EventBundle events) {
-        for (Event event : events) {
-            if (acceptEvent(event)) {
-                handleEvent(event);
-            }
-        }
-    }
-
-    @Override
-    public boolean acceptEvent(Event event) {
-        return handled.contains(event.getName());
-    }
+public class InddPreviewGeneration implements EventListener {
 
     public void handleEvent(Event event) {
         EventContext ctx = event.getContext();
         if (!(ctx instanceof DocumentEventContext)) {
-          return;
+            return;
         }
 
         DocumentEventContext docCtx = (DocumentEventContext) ctx;
         DocumentModel doc = docCtx.getSourceDocument();
 
+        if (!"File".equals(doc.getType())) {
+            return;
+        }
+
         boolean fileIsDirty = doc.getProperty("file:content").isDirty();
 
         Blob blob = (Blob) doc.getPropertyValue("file:content");
 
-        boolean isIndd = blob!= null && blob.getFilename().toLowerCase().endsWith(".indd");
+        boolean isIndd = blob != null && blob.getFilename().toLowerCase().endsWith(".indd");
 
-        if ("File".equals(doc.getType()) && fileIsDirty && isIndd) {
+        if (fileIsDirty && isIndd) {
             InddPreviewWorker work = new InddPreviewWorker(doc.getRepositoryName(), doc.getId(), "file:content");
             WorkManager workManager = Framework.getService(WorkManager.class);
             workManager.schedule(work, WorkManager.Scheduling.IF_NOT_SCHEDULED, true);
