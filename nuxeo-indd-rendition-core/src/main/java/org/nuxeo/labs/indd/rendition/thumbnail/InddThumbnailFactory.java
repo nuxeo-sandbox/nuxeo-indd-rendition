8242fc323b284ae8f7373a6effb3cd68f1dd9292
Michaël Vachette <mvachette@nuxeo.com>
2018-07-26 20:46:30 -0700

    Return default thumbnail if not indd file

--- nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/thumbnail/InddThumbnailFactory.java
+++ nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/thumbnail/InddThumbnailFactory.java
@@ -3,14 +3,14 @@ package org.nuxeo.labs.indd.rendition.thumbnail;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.thumbnail.ThumbnailFactory;
 import org.nuxeo.ecm.platform.thumbnail.ThumbnailConstants;
+import org.nuxeo.ecm.platform.thumbnail.factories.ThumbnailDocumentFactory;
 import org.nuxeo.labs.indd.rendition.service.InDesignRenditionService;
 import org.nuxeo.runtime.api.Framework;
 
 import java.util.List;
 
-public class InddThumbnailFactory implements ThumbnailFactory {
+public class InddThumbnailFactory extends ThumbnailDocumentFactory {
 
     @Override
     public Blob getThumbnail(DocumentModel doc, CoreSession session) {
@@ -26,7 +26,7 @@ public class InddThumbnailFactory implements ThumbnailFactory {
         Blob blob = (Blob) doc.getPropertyValue("file:content");
 
         if (blob == null || !"application/x-indesign".equals(blob.getMimeType())) {
-            return null;
+            return super.computeThumbnail(doc,session);
         }
 
         InDesignRenditionService inDesignRenditionService = Framework.getService(InDesignRenditionService.class);
