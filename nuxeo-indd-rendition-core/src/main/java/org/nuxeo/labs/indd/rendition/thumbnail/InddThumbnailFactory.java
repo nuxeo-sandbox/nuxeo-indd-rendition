453c81966a3088583a3a1a2136c47f8fd6ce319e
michael <mvachette@nuxeo.com>
2019-09-18 09:51:58 +0900

    Don't override getThumbnail method

--- nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/thumbnail/InddThumbnailFactory.java
+++ nuxeo-indd-rendition-core/src/main/java/org/nuxeo/labs/indd/rendition/thumbnail/InddThumbnailFactory.java
@@ -3,7 +3,6 @@ package org.nuxeo.labs.indd.rendition.thumbnail;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.platform.thumbnail.ThumbnailConstants;
 import org.nuxeo.ecm.platform.thumbnail.factories.ThumbnailDocumentFactory;
 import org.nuxeo.labs.indd.rendition.service.InDesignRenditionService;
 import org.nuxeo.runtime.api.Framework;
@@ -11,18 +10,6 @@ import org.nuxeo.runtime.api.Framework;
 import java.util.List;
 
 public class InddThumbnailFactory extends ThumbnailDocumentFactory {
-
-    @Override
-    public Blob getThumbnail(DocumentModel doc, CoreSession session) {
-        Blob thumbnailBlob = null;
-        if (doc.hasFacet(ThumbnailConstants.THUMBNAIL_FACET)) {
-            thumbnailBlob = (Blob) doc.getPropertyValue(ThumbnailConstants.THUMBNAIL_PROPERTY_NAME);
-        }
-        if (thumbnailBlob == null) {
-            thumbnailBlob = getDefaultThumbnail(doc);
-        }
-        return thumbnailBlob;
-    }
 
     @Override
     public Blob computeThumbnail(DocumentModel doc, CoreSession session) {
